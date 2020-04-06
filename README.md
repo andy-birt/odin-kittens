dc6da7578dc0f1e8fad13c8bee56cac07da1c42b
Andrew Birt <abirt1986@gmail.com>
2020-04-06 02:49:23 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Odin Kittens
+
+Building a RESTful resource for the sake of kittens.
+
+It will use HTML but also provide JSON data to be manipulated by the user.
---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,43 @@
+class KittensController < ApplicationController
+  
+  def index
+    @kittens = Kitten.all
+  end
+
+  def show
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def new
+    @kitten = Kitten.new
+  end
+
+  def create
+    @kitten = Kitten.new(kitten_params)
+    if @kitten.save
+      flash.success = 'Good Kitty!!'
+      redirect_to @kitten
+    else
+      flash.error = 'Bad kitty!'
+      render 'new'
+    end
+  end
+
+  def edit
+    @kitten = Kitten.find(params[:id])
+  end
+
+  def update
+    
+  end
+
+  def destroy
+  end
+
+  private
+
+    def kitten_params
+      params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+
+end
